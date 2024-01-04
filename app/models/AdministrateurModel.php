24f21bc5e6b1faab7d6e091f1c8c530b45d38b8d
caity <fdshjgjgffjhd@gmail.com>
2024-01-04 09:39:00 +0100

    add users and edit role

--- app/models/AdministrateurModel.php
+++ app/models/AdministrateurModel.php
@@ -1,40 +1,101 @@
 <?php
+
 namespace App\models;
 
 use App\core\Database;
+use PDO;
 
 
-class AdministrateurModel extends Database
+trait AdministrateurModel 
 {
+    use Database;
+    private $connection;
+    private $id;
+    private $nom;
+    private $prenom;
+    private  $email;
+    private $id_role;
+    private $titre;
+ 
+    public function getId() {
+        return $this->id;
+    }
+
+    public function setId($id) {
+        $this->id = $id;
+    }
+
+    public function getNom() {
+        return $this->nom;
+    }
+
+    public function setNom($nom) {
+        $this->nom = $nom;
+    }
+
+    public function getPrenom() {
+        return $this->prenom;
+    }
+
+    public function setPrenom($prenom) {
+        $this->prenom = $prenom;
+    }
+
+    public function getEmail() {
+        return $this->email;
+    }
+
+    public function setEmail($email) {
+        $this->email = $email;
+    }
+
+    public function getIdRole() {
+        return $this->id_role;
+    }
+
+    public function setIdRole($id_role) {
+        $this->id_role = $id_role;
+    }
+
+    public function getTitre() {
+        return $this->titre;
+    }
+
+    public function setTitre($titre) {
+        $this->titre = $titre;
+    }
+
+
+    public function __construct()
+    {
+        $this->connection = $this->connect();
+    }
     public function getUers()
     {
-        $con = $this->connect();
-        $sql = $con->prepare("SELECT u.id, u.nom, u.prenom, u.email,  u.id_role, r.titre
+        
+        $sql =  $this->connection->prepare("SELECT u.id, u.nom, u.prenom, u.email,  u.id_role, r.titre
         FROM users u
         JOIN roles r ON u.id_role = r.id");
         $sql->execute();
         $uers = $sql->fetchAll(PDO::FETCH_OBJ);
-        $this->dbConn = null;
         return $uers;
     }
     public function getRoles()
     {
-        $con = $this->connect();
-        $sql = $con->prepare("SELECT `id`, `titre` FROM `roles`");
+        $sql = $this->connection->prepare("SELECT `id`, `titre` FROM `roles`");
         $sql->execute();
         $roles = $sql->fetchAll(PDO::FETCH_OBJ);
-        $this->dbConn = null;
         return $roles;
     }
 
-    public function modifierRole(int $idU, int $idRole)
+    public function modifierRole()
     {
-        $con = $this->connect();
-        $sql = $con->prepare("UPDATE `users` SET  `id_role`= :a WHERE  `id` = :b");
-        $sql->bindParam(':b', $idU);
-        $sql->bindParam(':a', $idRole);
+        $id =  $this->getId();
+        $idRole = $this->getIdRole();
+        $sql = $this->connection->prepare("UPDATE `users` SET  `id_role`= :a WHERE  `id` = :b");
+        $sql->bindParam(':b',$id);
+        $sql->bindParam(':a',$idRole );
         $sql->execute();
-        $this->dbConn = null;
     }
 
     //     public function supprimerUer(int $idU)
@@ -64,11 +125,9 @@ class AdministrateurModel extends Database
 
     public function getReservation()
     {
-        $con = $this->connect();
-        $sql = $con->prepare("SELECT `date_de_creation`, `prixTotal` FROM `reservation` WHERE 1");
+        $sql = $this->connection->prepare("SELECT `date_de_creation`, `prixTotal` FROM `reservation` WHERE 1");
         $sql->execute();
         $reservation = $sql->fetchAll(PDO::FETCH_ASSOC);
-        $this->dbConn = null;
         return $reservation;
     }
 }
